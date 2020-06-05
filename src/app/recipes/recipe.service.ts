3b16e4894bcf7e19426a9f0425e1090d0f79970a
Jeremy Hansen <jhansen1344@gmail.com>
2020-06-05 15:42:53 -0400

    end of section 16

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,15 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
-recipeSelected = new EventEmitter<Recipe>();
+  recipesChanged = new Subject<Recipe[]>();
 constructor(private slService: ShoppingListService) { }
 private recipes: Recipe[] = [
   new Recipe(
@@ -38,5 +40,21 @@ private recipes: Recipe[] = [
 
   addIngredientsToShoppingList(ingredients: Ingredient[]){
     this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe){
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number){
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,11 +1,13 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
- ingredientsChanged = new EventEmitter<Ingredient[]>();
+ ingredientsChanged = new Subject<Ingredient[]>();
+ startedEditing = new Subject<number>();d
 constructor() { }
 
 private ingredients: Ingredient[] = [
@@ -18,15 +20,29 @@ getIngredients(){
   return this.ingredients.slice();
 }
 
+getIngredient(index: number){
+  return this.ingredients[index];
+}
+
 addIngredient(ingredient: Ingredient)
 {
   this.ingredients.push(ingredient);
-  this.ingredientsChanged.emit(this.ingredients.slice());
+  this.ingredientsChanged.next(this.ingredients.slice());
 }
 
 addIngredients(ingredients: Ingredient[]){
   this.ingredients.push(...ingredients);
-  this.ingredientsChanged.emit(this.ingredients.slice());
+  this.ingredientsChanged.next(this.ingredients.slice());
+}
+
+updateIngredient(index: number, newIngredient: Ingredient){
+  this.ingredients[index] = newIngredient;
+  this.ingredientsChanged.next(this.ingredients.slice());
+}
+
+deleteIngredient(index: number){
+  this.ingredients.splice(index, 1);
+  this.ingredientsChanged.next(this.ingredients.slice());
 }
 
 }
